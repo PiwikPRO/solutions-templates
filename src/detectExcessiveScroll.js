7ae9fccd0c7917dd35e16ab02656ecddafd8f6e7
Arek Zając <arekzc@gmail.com>
2021-03-10 10:18:22 +0100

    fix: excessive scroll direction change detection

--- src/detectExcessiveScroll.js
+++ src/detectExcessiveScroll.js
@@ -1,16 +1,19 @@
 export default (subscribe, { threshold }) => {
   let lastKnownScrollPosition = 0;
+  let lastKnownDirection = 0;
   let directionChangeCount = 0;
   let ticking = false;
 
   const listener = () => {
     const prevScrollPosition = lastKnownScrollPosition;
+    const prevScrollDirection = lastKnownDirection;
 
     lastKnownScrollPosition = window.scrollY;
+    lastKnownDirection = Math.sign(lastKnownScrollPosition - prevScrollPosition);
 
     if (!ticking) {
       window.requestAnimationFrame(() => {
-        if (lastKnownScrollPosition - prevScrollPosition > 0) {
+        if (lastKnownDirection != prevScrollDirection) {
           directionChangeCount++;
         }
 
