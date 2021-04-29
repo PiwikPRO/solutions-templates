97d36fad4c21e7944bcdfc1fb89962649ca2b355
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-29 10:25:58 +0000

    Deploy PiwikPRO/solutions-templates to github.com/PiwikPRO/solutions-templates.git:gh-pages

--- 935f2afb.abbf3e69.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{73:function(e){e.exports=JSON.parse('{"pluginId":"default","version":"current","label":"Next","isLast":true,"docsSidebars":{},"permalinkToSidebar":{}}')}}]);
