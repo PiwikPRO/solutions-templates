60760d10c891d51c44db0af4f8487c4a02e3e3cc
Michal Gonera <m.gonera@piwik.pro>
2021-06-10 15:27:34 +0200

    included comments for review

--- src/videoTrackingHTML5.js
+++ src/videoTrackingHTML5.js
@@ -3,7 +3,7 @@
  * 
  * This script analyses the interactions with <video> elements.
  */
-
+//4Patryk: I'm not sure whether to put "subscribe" here at all?
  export default (subscribe, {eventCategoryLabel,
                             videoTitleAttribute,
                             trackingAccuracy,
@@ -176,6 +176,7 @@
     const handleTabUnloadDuringVideoPlay = (e) => {
         mediaElements = document.querySelectorAll("video");
         mediaElements.forEach((mediaElement) => {
+            //4Patryk: I didn't have better idea for getting around eslint, it has issue with me checking for my own hasPlayed property on an object (it's added at the beginning of code execution)
             // eslint-disable-next-line @typescript-eslint/ban-ts-comment
             // @ts-ignore
             if (mediaElement.ended == false && mediaElement.paused == false && mediaElement.hasPlayed == true){
